f40159ab85d09dbba01bd3c27d886269c0cb2ff7
startdusk <clylia217@gmail.com>
2022-09-07 21:47:36 +0800

    feat✨:  If Expressions parser

--- ast/ast.go
+++ ast/ast.go
@@ -207,3 +207,51 @@ func (b *Boolean) String() string {
 }
 
 func (b *Boolean) expressionNode() {}
+
+type IfExpression struct {
+	Token       token.Token // The 'if' token
+	Condition   Expression
+	Consequence *BlockStatement
+	Alternative *BlockStatement
+}
+
+func (ie *IfExpression) TokenLiteral() string {
+	return ie.Token.Literal
+}
+
+func (ie *IfExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("if")
+	out.WriteString(" " + ie.Condition.String() + " ")
+	out.WriteString(ie.Condition.String())
+	if ie.Alternative != nil {
+		out.WriteString("else ")
+		out.WriteString(ie.Alternative.String())
+	}
+
+	return out.String()
+}
+
+func (ie *IfExpression) expressionNode() {}
+
+type BlockStatement struct {
+	Token      token.Token // The '{' token
+	Statements []Statement
+}
+
+func (bs *BlockStatement) TokenLiteral() string {
+	return bs.Token.Literal
+}
+
+func (bs *BlockStatement) String() string {
+	var out bytes.Buffer
+
+	for _, s := range bs.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
+}
+
+func (bs *BlockStatement) statementNode() {}
