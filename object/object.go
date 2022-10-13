67dc80748bc14b634a64c6da03b534939239bf96
startdusk <522383345@qq.com>
2022-10-13 19:59:26 +0800

    feat✨:  add object system

---
+++ object/object.go
@@ -0,0 +1,56 @@
+package object
+
+import "fmt"
+
+type ObjectType string
+
+const (
+	INTEGER_OBJ ObjectType = "INTEGER"
+	BOOLEAN_OBJ ObjectType = "BOOLEAN"
+	NULL_OBJ    ObjectType = "NULL"
+)
+
+type Object interface {
+	Type() ObjectType
+	Inspect() string
+}
+
+// =================================================================================================
+// Integer Object
+type Integer struct {
+	Value int64
+}
+
+func (i *Integer) Inspect() string {
+	return fmt.Sprintf("%d", i.Value)
+}
+
+func (i *Integer) Type() ObjectType {
+	return INTEGER_OBJ
+}
+
+// =================================================================================================
+// Boolean Object
+type Boolean struct {
+	Value bool
+}
+
+func (b *Boolean) Inspect() string {
+	return fmt.Sprintf("%t", b.Value)
+}
+
+func (b *Boolean) Type() ObjectType {
+	return BOOLEAN_OBJ
+}
+
+// =================================================================================================
+// Null Object
+type Null struct{}
+
+func (n *Null) Inspect() string {
+	return "null"
+}
+
+func (n *Null) Type() ObjectType {
+	return NULL_OBJ
+}
