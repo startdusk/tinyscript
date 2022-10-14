b49c6b99a6cad9125811a9f79c5b24c59b7a392d
startdusk <522383345@qq.com>
2022-10-14 21:13:14 +0800

    feat✨:  bang and mins operand

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -23,9 +23,45 @@ func Eval(node ast.Node) object.Object {
 		return &object.Integer{Value: node.Value}
 	case *ast.Boolean:
 		return nativeBoolToBooleanObject(node.Value)
+	case *ast.PrefixExpression:
+		right := Eval(node.Right)
+		return evalPirefixExpression(node.Operator, right)
 	}
 
 	return nil
+}
+
+func evalPirefixExpression(operator string, right object.Object) object.Object {
+	switch operator {
+	case "!":
+		return evalBangOperatorExpression(right)
+	case "-":
+		return evalMinsOperatorExpression(right)
+	default:
+		return NULL
+	}
+}
+
+func evalMinsOperatorExpression(right object.Object) object.Object {
+	if right.Type() != object.INTEGER_OBJ {
+		return NULL
+	}
+
+	value := right.(*object.Integer).Value
+	return &object.Integer{Value: -value}
+}
+
+func evalBangOperatorExpression(right object.Object) object.Object {
+	switch right {
+	case TRUE:
+		return FALSE
+	case FALSE:
+		return TRUE
+	case NULL:
+		return TRUE
+	default:
+		return FALSE
+	}
 }
 
 func evalStatements(stmts []ast.Statement) object.Object {
--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -14,8 +14,9 @@ func TestEvalIntergerExpression(t *testing.T) {
 		expected int64
 	}{
 		{"5", 5},
+		{"-5", -5},
 		{"10", 10},
-		// {"-10", -10}, not supported yet
+		{"-10", -10},
 	}
 
 	for _, tt := range tests {
@@ -75,4 +76,23 @@ func testBooleanObject(t *testing.T, obj object.Object, expected bool) bool {
 	}
 
 	return true
+}
+
+func TestBangOperator(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		{"!true", false},
+		{"!false", true},
+		{"!5", false},
+		{"!!true", true},
+		{"!!false", false},
+		{"!!5", true},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testBooleanObject(t, evaluated, tt.expected)
+	}
 }
