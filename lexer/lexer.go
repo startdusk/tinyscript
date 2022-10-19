04ddcf4e751dac23bc2e996b3ef4dbab7b609998
startdusk <522383345@qq.com>
2022-10-19 21:54:36 +0800

    feat✨:  lexer string

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -63,6 +63,9 @@ func (l *Lexer) NextToken() token.Token {
 		tok = newToken(token.LBRACE, l.ch)
 	case '}':
 		tok = newToken(token.RBRACE, l.ch)
+	case '"':
+		tok.Type = token.STRING
+		tok.Literal = l.readString()
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
@@ -82,6 +85,17 @@ func (l *Lexer) NextToken() token.Token {
 
 	l.readChar()
 	return tok
+}
+
+func (l *Lexer) readString() string {
+	pos := l.position + 1
+	for {
+		l.readChar()
+		if l.ch == '"' || l.ch == 0 {
+			break
+		}
+	}
+	return l.input[pos:l.position]
 }
 
 func (l *Lexer) readChar() {
--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -59,6 +59,8 @@ func TestNextToken(t *testing.T) {
 				}
 				10 == 10;
 				10 != 9;
+				"foobar"
+				"foo bar"
 			`,
 			expects: []expect{
 				{token.LET, "let"},
@@ -122,6 +124,8 @@ func TestNextToken(t *testing.T) {
 				{token.NOT_EQ, "!="},
 				{token.INT, "9"},
 				{token.SEMICOLON, ";"},
+				{token.STRING, "foobar"},
+				{token.STRING, "foo bar"},
 				{token.EOF, ""},
 			},
 		},
