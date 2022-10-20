305db9ce300e9a5d20a8e596d5d43cb01a47079b
startdusk <522383345@qq.com>
2022-10-20 20:53:50 +0800

    feat✨:  string concatenaton

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -223,6 +223,10 @@ func TestErrorHandling(t *testing.T) {
 			"foobar",
 			"identifier not found: foobar",
 		},
+		{
+			`"Hello" - "World"`,
+			"unknown operator: STRING - STRING",
+		},
 	}
 
 	for _, tt := range tests {
@@ -309,6 +313,20 @@ func TestClosures(t *testing.T) {
 
 func TestStringLiteral(t *testing.T) {
 	input := `"Hello World"`
+
+	evaluated := testEval(input)
+	str, ok := evaluated.(*object.String)
+	if !ok {
+		t.Fatalf("object is not String. got=%T(%+v)", evaluated, evaluated)
+	}
+
+	if str.Value != "Hello World" {
+		t.Fatalf("String has wrong value. got=%q", str.Value)
+	}
+}
+
+func TestStringConcatenation(t *testing.T) {
+	input := `"Hello" + " " + "World"`
 
 	evaluated := testEval(input)
 	str, ok := evaluated.(*object.String)
