933ff99e4a079c984bdbe4300a1188bd48fb923e
startdusk <522383345@qq.com>
2022-10-22 08:18:13 +0800

    feat✨:  array token

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -61,6 +61,7 @@ func TestNextToken(t *testing.T) {
 				10 != 9;
 				"foobar"
 				"foo bar"
+				[1, 2];
 			`,
 			expects: []expect{
 				{token.LET, "let"},
@@ -126,6 +127,12 @@ func TestNextToken(t *testing.T) {
 				{token.SEMICOLON, ";"},
 				{token.STRING, "foobar"},
 				{token.STRING, "foo bar"},
+				{token.LBRACKET, "["},
+				{token.INT, "1"},
+				{token.COMMA, ","},
+				{token.INT, "2"},
+				{token.RBRACKET, "]"},
+				{token.SEMICOLON, ";"},
 				{token.EOF, ""},
 			},
 		},
