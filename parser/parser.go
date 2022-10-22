6b655e211f7619127e54fb1d6bf4c829862f80eb
startdusk <522383345@qq.com>
2022-10-22 09:40:47 +0800

    feat✨:  parsing index operator expressions

--- parser/parser.go
+++ parser/parser.go
@@ -18,6 +18,7 @@ const (
 	PRODUCT     // *
 	PREFIX      // -X or !X
 	CALL        // myFunction(X)
+	INDEX
 )
 
 var precedences = map[token.TokenType]int{
@@ -30,6 +31,7 @@ var precedences = map[token.TokenType]int{
 	token.SLASH:    PRODUCT,
 	token.ASTERISK: PRODUCT,
 	token.LPAREN:   CALL,
+	token.LBRACKET: INDEX,
 }
 
 type Parser struct {
@@ -77,6 +79,7 @@ func New(l *lexer.Lexer) *Parser {
 		p.registerInfix(token.LT, p.parseInfixExpression)
 		p.registerInfix(token.GT, p.parseInfixExpression)
 		p.registerInfix(token.LPAREN, p.parseCallExpression)
+		p.registerInfix(token.LBRACKET, p.parseIndexExpression)
 	}
 
 	return &p
@@ -108,6 +111,17 @@ func (p *Parser) ParseProgram() *ast.Program {
 	}
 
 	return program
+}
+
+func (p *Parser) parseIndexExpression(left ast.Expression) ast.Expression {
+	exp := ast.IndexExpression{Token: p.curToken, Left: left}
+
+	p.nextToken()
+	exp.Index = p.parseExpression(LOWEST)
+	if !p.expectPeek(token.RBRACKET) {
+		return nil
+	}
+	return &exp
 }
 
 func (p *Parser) parseArrayLiteral() ast.Expression {
